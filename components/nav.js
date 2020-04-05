4b68243ed8e553e4e859b4deb7328e725d0e8b0f
Eric Jiang <ericjiang97@gmail.com>
2020-04-06 09:25:55 +1000

    remove old links in navbar

--- components/nav.js
+++ components/nav.js
@@ -32,18 +32,6 @@ const Nav = () => {
       >
         <div class="text-sm lg:flex-grow">
           <a
-            href="#responsive-header"
-            class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-blue-700 mr-4"
-          >
-            Docs
-          </a>
-          <a
-            href="#responsive-header"
-            class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-blue-700 mr-4"
-          >
-            Examples
-          </a>
-          <a
             href="/blog"
             class="block mt-4 lg:inline-block lg:mt-0 text-teal-500 hover:text-blue-700"
           >
