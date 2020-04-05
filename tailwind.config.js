f3d487f4387466b76581b679603cf7ce88ba75f7
Eric Jiang <ericjiang97@gmail.com>
2020-04-05 21:34:43 +1000

    hopefully this fixes font issue

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: [
+        "Inter",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        '"Noto Sans"',
+        "sans-serif",
+      ],
+      seif: [
+        "Inter",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        '"Noto Sans"',
+        "sans-serif",
+      ],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
