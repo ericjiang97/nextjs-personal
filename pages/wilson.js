64b6e6a843ccc01a9a869e6f3eceff072b6d46fa
Eric Jiang <ericjiang97@gmail.com>
2020-04-05 22:12:31 +1000

    secret page

---
+++ pages/wilson.js
@@ -0,0 +1,10 @@
+const Wilson = () => {
+  return (
+    <div>
+      Hello Wilson
+      <img src="https://media-exp1.licdn.com/dms/image/C5603AQFCqARWQfNFjg/profile-displayphoto-shrink_800_800/0?e=1591833600&v=beta&t=tDE7jehEfLHHXDVXvxfauaLuw6zJSRNbgM4liBGr7ao" />
+    </div>
+  );
+};
+
+export default Wilson;
