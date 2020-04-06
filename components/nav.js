c79c60a1634c2d8e6716d0b85bfc5000afb963aa
Eric Jiang <ericjiang97@gmail.com>
2020-04-06 10:16:20 +1000

    nextjs cleanup

--- components/nav.js
+++ components/nav.js
@@ -4,19 +4,19 @@ import Link from "next/link";
 const Nav = () => {
   const [expanded, setExpanded] = useState(false);
   return (
-    <nav class="sticky top-0 flex items-center justify-between bg-blue-100 flex-wrap p-6">
-      <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <nav className="sticky top-0 flex items-center justify-between bg-blue-100 flex-wrap p-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
         <Link href="/">
           <a className="text-blue-500 no-underline text-lg">Eric Jiang</a>
         </Link>
       </div>
-      <div class="block lg:hidden">
+      <div className="block lg:hidden">
         <button
-          class="flex items-center px-3 py-2 border rounded text-blue-500 border-blue-400 hover:text-blue-700 hover:border-blue-700"
+          className="flex items-center px-3 py-2 border rounded text-blue-500 border-blue-400 hover:text-blue-700 hover:border-blue-700"
           onClick={() => setExpanded(!expanded)}
         >
           <svg
-            class="fill-current h-3 w-3"
+            className="fill-current h-3 w-3"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
@@ -26,14 +26,14 @@ const Nav = () => {
         </button>
       </div>
       <div
-        class={`w-full block flex-grow lg:flex md:items-end lg:w-auto ${
+        className={`w-full block flex-grow lg:flex md:items-end lg:w-auto ${
           !expanded && "hidden"
         }`}
       >
-        <div class="text-sm lg:flex-grow">
+        <div className="text-sm lg:flex-grow">
           <a
             href="/blog"
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-500 hover:text-blue-700"
+            className="block mt-4 lg:inline-block lg:mt-0 text-teal-500 hover:text-blue-700"
           >
             Blog
           </a>
