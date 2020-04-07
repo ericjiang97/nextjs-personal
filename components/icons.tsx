b7f7a1a261e6abf52b159a17d8c8665ad5fe0767
Eric Jiang <ericjiang97@gmail.com>
2020-04-07 13:31:44 +1000

    moved icons to typescript

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SunIcon = ({ className }) => {
+const SunIcon: React.FC<{ className: string }> = ({ className }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -13,7 +13,7 @@ const SunIcon = ({ className }) => {
   );
 };
 
-const MoonIcon = ({ className }) => {
+const MoonIcon: React.FC<{ className: string }> = ({ className }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
