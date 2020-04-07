dbd55541267a2d6596452fafaa518df011b999d7
Eric Jiang <ericjiang97@gmail.com>
2020-04-07 14:59:05 +1000

    wordpress api model

---
+++ types/wordpress_api.d.ts
@@ -0,0 +1,29 @@
+interface ContentObject {
+  //This property is always present
+  rendered: string;
+  //This property is only present in some contexts
+  raw?: string;
+  // Is the Content Protected
+  protected: boolean;
+}
+
+export interface Post {
+  id: number;
+  slug: string;
+  date: string;
+  date_gmt: string;
+  title: {
+    //This property is always present
+    rendered: string;
+    //This property is only present in some contexts
+    raw?: string;
+  };
+  author_info: {
+    display_name: string;
+    author_link: string;
+  };
+  content: ContentObject;
+  excerpt: ContentObject;
+  uagb_excerpt: string;
+  status: "publish" | "future" | "draft" | "pending" | "private";
+}
