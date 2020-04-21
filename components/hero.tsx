3eec55c4d130c3abe2a32668862db73cf5e11cdc
Eric Jiang <ericjiang97@gmail.com>
2020-04-21 10:15:51 +1000

    updated hero banner

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,13 +2,7 @@ import React from 'react';
 
 const Hero = () => {
   const randomGreeting = () => {
-    const greetings = [
-      'Hello',
-      'Hello there',
-      'こんにちは',
-      '你好',
-      'Bounjour',
-    ];
+    const greetings = ['Hello', 'Hello there', 'こんにちは', '你好', 'Bounjour'];
     return greetings[Math.floor(Math.random() * greetings.length)];
   };
   return (
@@ -17,20 +11,13 @@ const Hero = () => {
         <h1 className="m-0 w-full pt-20 leading-tight text-4xl text-left font-bold">
           {`${randomGreeting()}, I'm Eric.`}
         </h1>
-        <h3 className="m-0 w-full pt-10 leading-tight text-3xl text-left font-bold">
-          I’m a Software Engineer
-        </h3>
+        <h3 className="m-0 w-full pt-10 leading-tight text-3xl text-left font-bold">I’m a Test Engineer at Google.</h3>
         <p className="text-left my-4 text-m">
-          And I make impact by building awesome software solutions and building
-          the communities around me.
+          In my spare time, I make impact by building awesome software solutions and building the communities around me.
         </p>
       </div>
       <div className="max-w-4xl mx-auto py-auto pb-8 flex flex-col justify-end">
-        <img
-          className="hidden md:inline"
-          src="/images/transparent_profile_min.png"
-          alt="Eric"
-        />
+        <img className="hidden md:inline" src="/images/transparent_profile_min.png" alt="Eric" />
       </div>
     </div>
   );
