d753c766700fed0292411c449281f2ddc0ac7051
Eric Jiang <ericjiang97@gmail.com>
2020-05-09 19:24:33 +1000

    added copy-to-clipboard button

--- components/renderers/CodeBlock.tsx
+++ components/renderers/CodeBlock.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
 import Highlight, { defaultProps, Language } from 'prism-react-renderer';
 import theme from 'prism-react-renderer/themes/nightOwl';
+import icons from '../icons';
 
 interface CodeBlockProps {
   language: Language;
@@ -13,7 +15,15 @@ const CodeBlock: React.FC<CodeBlockProps> = ({ language, value }) => {
       <Highlight {...defaultProps} code={value} language={language as Language} theme={theme}>
         {({ className, style, tokens, getLineProps, getTokenProps }) => (
           <pre className={`${className} py-2 px-2 relative`} style={style}>
-            <div className="absolute top-0 right-0 px-2 py-1 bg-brand">{language}</div>
+            <div className="absolute top-0 right-0">
+              <div className="px-2 py-1 bg-brand">{language}</div>
+              <CopyToClipboard text={value}>
+                <button className="bg-brand hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2 w-full">
+                  <icons.Copy className="fill-current h-5 w-5 mx-2" />
+                </button>
+              </CopyToClipboard>
+            </div>
+
             {tokens.map((line, i) => (
               <div {...getLineProps({ line, key: i })}>
                 {line.map((token, key) => (
