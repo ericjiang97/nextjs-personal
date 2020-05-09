6900893d6786e5e1f99eb2960ba1e8c1d8b51f6b
Eric Jiang <ericjiang97@gmail.com>
2020-05-09 16:00:16 +1000

    site upgrades

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="max-w-4xl mx-auto py-auto flex flex-row justify-around">
       <div className="max-w-4xl mx-auto py-auto p-8 flex flex-col justify-around">
-        <h1 className="m-0 w-full pt-20 leading-tight text-4xl text-left font-bold">{`G'day, I'm Eric.`}</h1>
+        <h1 className="m-0 w-full pt-20 leading-tight text-4xl text-left font-bold">G'day, I'm Eric.</h1>
         <h3 className="m-0 w-full pt-10 leading-tight text-3xl text-left font-bold">I’m a Test Engineer at Google.</h3>
         <p className="text-left my-4 text-m">
           In my spare time, I make impact by building awesome software solutions and building the communities around me.
