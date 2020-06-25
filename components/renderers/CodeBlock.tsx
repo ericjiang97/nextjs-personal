00cf144c71c524d2ddcea970da855316e85da83d
Eric <ericjiang97@gmail.com>
2020-06-25 20:50:03 +1000

    fix codeblock overflow :)

--- components/renderers/CodeBlock.tsx
+++ components/renderers/CodeBlock.tsx
@@ -15,22 +15,31 @@ const CodeBlock: React.FC<CodeBlockProps> = ({ language, value }) => {
       <Highlight {...defaultProps} code={value} language={language as Language} theme={theme}>
         {({ className, style, tokens, getLineProps, getTokenProps }) => (
           <pre className={`${className} py-2 px-2 relative z-0`} style={style}>
-            <div className="absolute top-0 right-0">
-              <div className="px-2 py-1 bg-brand">{language}</div>
+            <div className="overflow-x-scroll py-2">
+              {tokens.map((line, i) => (
+                <div {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              ))}
+            </div>
+            <div className="flex justify-between flex-wrap">
+              <div
+                className="bg-brand hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2 w-full"
+                style={{ maxWidth: '6rem' }}
+              >
+                {language}
+              </div>
               <CopyToClipboard text={value}>
-                <button className="bg-brand hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2 w-full">
+                <button
+                  className="bg-brand hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2 w-full"
+                  style={{ maxWidth: '6rem' }}
+                >
                   <icons.Copy className="fill-current h-5 w-5 mx-2" />
                 </button>
               </CopyToClipboard>
             </div>
-
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            ))}
           </pre>
         )}
       </Highlight>
