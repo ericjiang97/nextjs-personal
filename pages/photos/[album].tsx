30fa8995ab4f50ed76526ae8bd991c3bfba72003
Eric <ericjiang97@gmail.com>
2020-06-27 13:57:08 +1000

    adding more prop checks

--- pages/photos/[album].tsx
+++ pages/photos/[album].tsx
@@ -17,8 +17,8 @@ export default function AlbumTemplate(props: AlbumTemplateProps) {
     <PageLayout title={`Photo - ${albumData.photoset && albumData.photoset.title}`}>
       <div className="w-full text-gray-900">
         <div className="max-w-4xl mx-auto py-auto pb-2 flex flex-col justify-around">
-          <h3 className="text-3xl font-semibold">{albumInfo.photoset.title._content}</h3>
-          <h3 className="text-2xl font-regular">{albumInfo.photoset.description._content}</h3>
+          <h3 className="text-3xl font-semibold">{albumInfo.photoset && albumInfo.photoset.title._content}</h3>
+          <h3 className="text-2xl font-regular">{albumInfo.photoset && albumInfo.photoset.description._content}</h3>
 
           <div className="flex-1 flex flex-col mt-2">
             {albumData.photoset.photo &&
