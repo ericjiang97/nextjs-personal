09093e498da0414987236b8fe81346f019d05d56
Eric Jiang <ericjiang97@gmail.com>
2020-07-12 14:28:37 +1000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ const RootComponent: React.FC = ({ children }) => {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
         <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
         <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
