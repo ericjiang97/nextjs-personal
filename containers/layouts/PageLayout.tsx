9401f9b4b70e859ea27fc337f9d19616b34d09cf
Eric <ericjiang97@gmail.com>
2020-07-26 15:59:13 +1000

    improve mobile

--- containers/layouts/PageLayout.tsx
+++ containers/layouts/PageLayout.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
-import { PageContent, PageWithHeader, Callout, Container, Link } from 'bumbag';
+import { PageContent, PageWithSidebar, PageWithHeader, Callout, Container, Link, useBreakpoint } from 'bumbag';
 import Nav from '../../components/nav';
+import SideBar from '../../components/SideBar';
 
 interface PageLayoutProps {
   title: string;
@@ -10,25 +11,45 @@ interface PageLayoutProps {
   ignoreHorizontalPadding?: boolean;
 }
 
-const PageLayout: React.FC<PageLayoutProps> = ({ title, banner, children }) => {
-  const titleString = `${title} - Eric Jiang`;
+interface PageInnerProps {
+  title?: string;
+  banner?: JSX.Element | JSX.Element[];
+}
+
+const PageChildren: React.FC<PageInnerProps> = ({ title, banner, children }) => {
   return (
-    <PageWithHeader header={<Nav />} display="flex" flexDirection="column">
+    <>
       <Head>
-        <title>{titleString}</title>
+        <title>{title}</title>
       </Head>
       {banner && banner}
       <div style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-
         <PageContent flex={1}>
           <Container>
             <Callout type="info" title="Warning: Experimental Site" maxWidth="800px" width="100%" marginY="1rem">
-              This site is experimental, please report any bugs to <Link href="https://github.com/ericjiang97/nextjs-personal/issues">https://github.com/ericjiang97/nextjs-personal/issues</Link>
+              This site is experimental, please report any bugs to{' '}
+              <Link href="https://github.com/ericjiang97/nextjs-personal/issues">
+                https://github.com/ericjiang97/nextjs-personal/issues
+              </Link>
             </Callout>
           </Container>
           {children}
         </PageContent>
       </div>
+    </>
+  );
+};
+
+const PageLayout: React.FC<PageLayoutProps> = ({ title, banner, children }) => {
+  const titleString = `${title} - Eric Jiang`;
+  const isTabletOrSmaller = useBreakpoint('max-tablet');
+  return (
+    <PageWithHeader header={<Nav />} display="flex" flexDirection="column" defaultIsVisible={true}>
+      <PageWithSidebar sidebar={<SideBar />} defaultIsVisible={isTabletOrSmaller}>
+        <PageChildren title={titleString} banner={banner}>
+          {children}
+        </PageChildren>
+      </PageWithSidebar>
     </PageWithHeader>
   );
 };
