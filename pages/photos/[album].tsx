f6bd37637150df5d80a3e2109b2d2858ac8d80da
Eric <ericjiang97@gmail.com>
2020-07-29 20:04:45 +1000

    changes to album page

--- pages/photos/[album].tsx
+++ pages/photos/[album].tsx
@@ -7,7 +7,7 @@ import Custom404 from '../404';
 import { photos } from '../../data/photos';
 import CONSTANTS from '../../constants';
 import { FlickrPhotoset, FlickrPhotosetInfo } from '../../types/FlickrApi';
-import { Heading, Paragraph, Stack, Button, Link, Container, Image } from 'bumbag';
+import { Heading, Paragraph, Stack, Button, Link, Container } from 'bumbag';
 import HeroBase from '../../components/HeroBase';
 
 export default function AlbumTemplate(props: InferGetStaticPropsType<typeof getServerSideProps>) {
@@ -48,21 +48,34 @@ export default function AlbumTemplate(props: InferGetStaticPropsType<typeof getS
       }
       inChildrenInContainer={false}
     >
-      <Stack>
+      <div
+        style={{
+          display: 'grid',
+          gridGap: '0.75rem',
+          gridTemplateColumns: 'repeat(auto-fill, minmax(250px,1fr))',
+          gridTemplateRows: 'repeat(auto-fill, minmax(250px,1fr))',
+        }}
+      >
         {albumData.photoset &&
           albumData.photoset.photo &&
           albumData.photoset.photo.map((photo, index) => {
+            const bgColor = '#09203390';
             return (
               <div
                 style={{
+                  backgroundImage: `linear-gradient(to top, ${bgColor}, ${bgColor}), url(${photo.url_m})`,
+                  backgroundPositionX: 'center',
+                  backgroundSize: 'cover',
+                  color: 'white',
                   display: 'flex',
                   flexDirection: index % 2 === 0 ? 'row' : 'row-reverse',
                   flexWrap: 'wrap',
+                  height: 400,
                   width: '100%',
                 }}
               >
-                <div style={{ flex: 1, minWidth: 280 }}>
-                  <Container flex={1} padding="0.75rem">
+                <Container flex={1} padding="0.75rem" display="flex" flexDirection="column">
+                  <div style={{ flex: 1 }}>
                     <Heading use="h5">{photo.title}</Heading>
                     <Paragraph fontSize="0.75rem">
                       <b style={{ fontWeight: 600 }}>Taken: </b>
@@ -76,18 +89,15 @@ export default function AlbumTemplate(props: InferGetStaticPropsType<typeof getS
                         <Paragraph fontSize="0.75rem">{photo.description._content}</Paragraph>
                       </>
                     )}
-                    <Link href={photo.url_o}>
-                      <Button iconBefore="solid-download">Download</Button>
-                    </Link>
-                  </Container>
-                </div>
-                <div style={{ flex: 1, minWidth: 280 }}>
-                  <Image src={photo.url_m} alt={photo.title} width="100%" />
-                </div>
+                  </div>
+                  <Link href={photo.url_o}>
+                    <Button iconBefore="solid-download">Download</Button>
+                  </Link>
+                </Container>
               </div>
             );
           })}
-      </Stack>
+      </div>
     </PageLayout>
   );
 }
