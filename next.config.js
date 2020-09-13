5b403d09100539c71f7c99836c0398ec7738baaa
Eric <ericjiang97@gmail.com>
2020-09-13 22:07:23 +1000

    PWA test

--- next.config.js
+++ next.config.js
@@ -3,11 +3,17 @@ const withFonts = require('next-fonts');
 const withMdxEnhanced = require('next-mdx-enhanced');
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
+const withPWA = require('next-pwa');
 
 module.exports = withPlugins([
   optimizedImages(),
   withCSS(),
   withFonts(),
+  withPWA({
+    pwa: {
+      dest: 'public',
+    },
+  }),
   withMdxEnhanced({
     layoutPath: 'layouts',
     defaultLayout: true,
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,6 +27,7 @@ const RootComponent: React.FC = ({ children }) => {
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <div>
         <div>{children}</div>
