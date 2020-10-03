59b77dd78aa66f1ca134e22f0486bc0efdd4f4d8
Eric <ericjiang97@gmail.com>
2020-10-03 11:43:21 +1000

    add fumushi inari wallpapers

--- data/wallpapers.ts
+++ data/wallpapers.ts
@@ -138,6 +138,63 @@ const wallpapers: WallpaperGroup[] = [
       },
     ],
   },
+  {
+    title: 'Fumushi Inari',
+    previewUrl: '/downloads/wallpapers/fumushi-inari-1/preview.webp',
+    wallpapers: [
+      {
+        title: 'XDR',
+        meta: {
+          colorType: 'P3',
+          hdRes: '6K',
+        },
+        resolution: '6016 x 3384',
+        downloadUrl: '/downloads/wallpapers/fumushi-inari-1/FumushiInari_ProXDR_P3_6016x3384.jpg',
+      },
+      {
+        title: 'iMac',
+        meta: {
+          colorType: 'P3',
+          hdRes: '5K',
+        },
+        resolution: '5120 x 2880',
+        downloadUrl: '/downloads/wallpapers/fumushi-inari-1/FumushiInari_iMacPro_P3_5120x2880.jpg',
+      },
+      {
+        title: 'MacBook Pro',
+        meta: {
+          colorType: 'P3',
+        },
+        resolution: '2560 x 1600',
+        downloadUrl: '/downloads/wallpapers/fumushi-inari-1/FumushiInari_MacbookProAIr_P3_2560x1600.jpg',
+      },
+      {
+        title: '4K Display',
+        meta: {
+          colorType: 'sRGB',
+          hdRes: '4K',
+        },
+        resolution: '3840 x 2160',
+        downloadUrl: '/downloads/wallpapers/fumushi-inari-1/FumushiInari_Desktop_4k_3840x2160.jpg',
+      },
+      {
+        title: '1440p Display',
+        meta: {
+          colorType: 'sRGB',
+        },
+        resolution: '2560 x 1440',
+        downloadUrl: '/downloads/wallpapers/fumushi-inari-1/FumushiInari_Desktop_SRGB_2560x1440.jpg',
+      },
+      {
+        title: '1080p Display',
+        meta: {
+          colorType: 'sRGB',
+        },
+        resolution: '1920 x 1080',
+        downloadUrl: '/downloads/wallpapers/fumushi-inari-1/FumushiInari_Desktop_SRGB_1920x1080.jpg',
+      },
+    ],
+  },
 ];
 
 export default wallpapers;
