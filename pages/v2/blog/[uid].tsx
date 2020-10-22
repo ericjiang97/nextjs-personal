8da0120f77a7537ae121d3d7194afbd6e0c2710a
Eric <ericjiang97@gmail.com>
2020-10-22 21:23:08 +1100

    make blog slightly better

--- pages/v2/blog/[uid].tsx
+++ pages/v2/blog/[uid].tsx
@@ -3,28 +3,48 @@ import moment from 'moment';
 import Prismic from 'prismic-javascript';
 
 import { RichText } from 'prismic-reactjs';
-import { Container, Heading, Label, Paragraph } from 'bumbag';
+import { Container, Heading, Icon, Label, Paragraph } from 'bumbag';
 
 import HeroBase from '../../../components/core/HeroBase';
+import ShareModal from '../../../components/modals/ShareModal';
 import PageLayout from '../../../containers/layouts/PageLayout';
 
 import { client } from '../../../config/prismic';
 
 export default function Post({ data, uid }: { data: any; uid: string }) {
+  const { title, author, preview, published_time } = data;
   return (
     <PageLayout
-      title={`Blog - ${data.title}`}
+      title={`Blog - ${title}`}
       ignoreHorizontalPadding={true}
       banner={
         <>
-          <HeroBase backgroundImage={`url('${data.banner}')`}>
-            <Heading use="h3">{RichText.asText(data.title)}</Heading>
+          {preview && (
+            <Container
+              backgroundColor="primary"
+              color="white"
+              width="100vw"
+              maxWidth="100vw"
+              paddingX="1rem"
+              paddingY="1.5rem"
+              display="flex"
+              alignItems="center"
+            >
+              <Icon icon="solid-info-circle" marginRight="1.5rem" />
+              <Paragraph marginTop="0">
+                This is a <strong>preview</strong> article used for testing and sharing purposes, please DO NOT share
+                this article until it has been published.
+              </Paragraph>
+            </Container>
+          )}
+          <HeroBase backgroundImage={`url('${data.banner.url}')`}>
+            <Heading use="h3">{RichText.asText(title)}</Heading>
             <Container marginY="1rem"></Container>
             <Container marginY="1rem">
               <Label>By</Label>
-              <Paragraph>{RichText.asText(data.author)}</Paragraph>
+              <Paragraph>{RichText.asText(author)}</Paragraph>
               <Label>Published on</Label>
-              <Paragraph>{moment(data.published_time).format('ddd Do MMM YYYY')}</Paragraph>
+              <Paragraph>{moment(published_time).format('ddd Do MMM YYYY')}</Paragraph>
             </Container>
           </HeroBase>
         </>
@@ -34,6 +54,9 @@ export default function Post({ data, uid }: { data: any; uid: string }) {
       }}
     >
       <Container>{RichText.render(data.body)}</Container>
+      <Container marginY="1rem" display="flex" flexWrap="wrap" justifyContent="space-between">
+        <ShareModal title={RichText.asText(title)} slug={`/blog/${uid}`} />
+      </Container>
     </PageLayout>
   );
 }
--- pages/v2/blog/index.tsx
+++ pages/v2/blog/index.tsx
@@ -1,21 +1,53 @@
-import Link from 'next/link';
 import Prismic from 'prismic-javascript';
+import moment from 'moment';
+
 import { RichText } from 'prismic-reactjs';
+
+import { Card, Heading, Image, Stack, Paragraph } from 'bumbag';
+import LinkButton from '../../../components/buttons/LinkButton';
+import HeroBase from '../../../components/core/HeroBase';
+import PageLayout from '../../../containers/layouts/PageLayout';
+
 import { client } from '../../../config/prismic';
 
 export default function BlogHome(props: any) {
   return (
-    <div>
-      <ul>
-        {props.posts.results.map((post: { uid: string; data: any }) => (
-          <li key={post.uid}>
-            <Link href={`/v2/blog/${post.uid}`} as={`/v2/blog/${post.uid}`}>
-              <a>{RichText.render(post.data.title)} </a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <PageLayout
+      title={'Blog'}
+      pageMeta={{
+        description: 'Blog',
+        endpoint: '/blog',
+      }}
+      banner={
+        <HeroBase backgroundImage="url(https://live.staticflickr.com/65535/49836502853_dd2b878f7b_b.jpg)">
+          <Heading use="h3">Blog</Heading>
+        </HeroBase>
+      }
+    >
+      <Stack>
+        {props.posts.results.map((post: { uid: string; data: any }) => {
+          const { uid, data } = post;
+          const { title, published_time, summary } = data;
+          return (
+            <Card standalone key={uid}>
+              {data.banner && (
+                <Image src={data.banner.url} alt={`cover image for ${RichText.asText(title)}`} width="100%" />
+              )}
+              <Card.Header flexDirection="column" alignItems="start">
+                <Card.Title fontPalette="primary">{RichText.asText(title)}</Card.Title>
+              </Card.Header>
+              <Card.Content>
+                <Heading use="h7">{moment(published_time).format('ddd Do MMM YYYY')}</Heading>
+                <Paragraph>{RichText.asText(summary)}</Paragraph>
+              </Card.Content>
+              <Card.Footer>
+                <LinkButton href={`/v2/blog/${uid}`}>Read Article</LinkButton>
+              </Card.Footer>
+            </Card>
+          );
+        })}
+      </Stack>
+    </PageLayout>
   );
 }
 
