84af45dd6fed2ac8eb4e52ae11cc7bd88a88fa86
Eric Jiang <ericjiang97@gmail.com>
2021-02-09 19:27:43 +0800

    improvements to BlogCard

--- components/blog/BlogCard/index.tsx
+++ components/blog/BlogCard/index.tsx
@@ -15,28 +15,30 @@ const BlogCard: React.FC<BlogCardProps> = ({
   showCoverImage = true,
   maxWordCount = undefined,
 }) => {
-  const { banner, title, summary, published_time, category } = blogContent;
+  const { banner, title, summary, published_time, category, body } = blogContent;
   const categoryLinkProps = Link.useProps({ href: `/blog/categories/${category.uid}` });
 
   let displayedSummary = RichText.asText(summary);
   if (maxWordCount) {
-    displayedSummary = `${RichText.asText(summary).split(' ').splice(0, maxWordCount).join(' ')}...`;
+    displayedSummary = displayedSummary
+      ? `${displayedSummary.split(' ').splice(0, maxWordCount).join(' ')}...`
+      : `${RichText.asText(body).split(' ').splice(0, maxWordCount).join(' ')}...`;
   }
 
   return (
-    <Card {...cardProps} minWidth={280} standalone>
+    <Card {...cardProps} minWidth={280} borderRadius="1" standalone>
       {showCoverImage && banner && banner.url && (
         <Box display="flex" justifyContent="center">
           <Image src={banner.url} alt={`cover image for ${RichText.asText(title)}`} maxHeight="300px" />
         </Box>
       )}
       <Card.Header flexDirection="column" alignItems="start">
-        <Tag use={Link} {...categoryLinkProps} palette="primary" variant="outlined" size="small" marginY="0.5rem">
+        <Tag use={Link} {...categoryLinkProps} palette="coral" size="small" marginY="0.5rem" color="textPrimary">
           {category.data.category_name}
         </Tag>
         <Card.Title fontPalette="primary">{RichText.asText(title)}</Card.Title>
       </Card.Header>
-      <Card.Content>{showSummary && <Paragraph>{displayedSummary}</Paragraph>}</Card.Content>
+      <Card.Content>{showSummary && displayedSummary && <Paragraph>{displayedSummary}</Paragraph>}</Card.Content>
       <Card.Footer>
         <LinkButton href={`/blog/${uid}`}>Read Article</LinkButton>
         <Heading fontSize="150" marginTop="1.5rem" color="primary300">
