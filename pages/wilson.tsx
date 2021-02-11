53578af7b27d58579e1b99c82698fd3ac3b6fdae
Eric Jiang <ericjiang97@gmail.com>
2021-02-11 14:31:23 +0800

    wat

--- pages/wilson.tsx
+++ pages/wilson.tsx
@@ -1,9 +1,26 @@
+import { Container, Heading, Image } from 'bumbag';
+import HeroBase from '../components/core/HeroBase';
+import PageLayout from '../containers/layouts/PageLayout';
+
 const Wilson = () => {
   return (
-    <div>
-      Hello Wilson
-      <img src="https://media-exp1.licdn.com/dms/image/C5603AQFCqARWQfNFjg/profile-displayphoto-shrink_800_800/0?e=1591833600&v=beta&t=tDE7jehEfLHHXDVXvxfauaLuw6zJSRNbgM4liBGr7ao" />
-    </div>
+    <PageLayout
+      title="Hello Wilson"
+      banner={
+        <HeroBase backgroundVariant="color" backgroundColor="primary600">
+          <Container breakpoint="desktop">
+            <Heading>Hello Wilson</Heading>
+            <Image
+              width="100%"
+              src="https://media-exp1.licdn.com/dms/image/C5603AQHbiO77Tv-JVQ/profile-displayphoto-shrink_200_200/0/1598670201624?e=1617840000&v=beta&t=3bK_Lh6oDa7Zvd9NB6-jrHUmA6q8SKFMmZK3Dum38fw"
+            />
+          </Container>
+        </HeroBase>
+      }
+      pageMeta={{
+        endpoint: '/wilson',
+      }}
+    ></PageLayout>
   );
 };
 
