453dedb319db9fd0018af9679ea5c3943f92b4bc
Eric Jiang <eric@ericjiang.dev>
2022-04-04 11:33:07 +0800

    remove prismic

--- .slicemachine/assets/slices/TestSlicer/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/TestSlicer';
-
-export default {
-  title: 'slices/TestSlicer'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"test_slicer","items":[],"primary":{"title":[{"type":"heading1","text":"Recontextualize killer eyeballs","spans":[]}],"description":[{"type":"paragraph","text":"Eu dolor ea amet veniam duis exercitation excepteur minim esse cupidatat ullamco Lorem dolore ipsum. Consequat excepteur non elit et nostrud ex nulla nisi reprehenderit. Veniam ad dolor qui reprehenderit nostrud aute amet sit Lorem deserunt dolor.","spans":[]}]},"id":"_Default"}} />
-_Default.storyName = 'Default'
