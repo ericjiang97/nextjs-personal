72671a5a0225becfdb4c377e63dbb7a84fe61a9e
Eric Jiang <eric@ericjiang.dev>
2022-04-04 11:42:26 +0800

    fix issues

--- components/HeroBanner/index.tsx
+++ components/HeroBanner/index.tsx
@@ -6,11 +6,11 @@ export default function Hero() {
                     <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
                         <div className="lg:py-24">
                             <h1 className="mt-4 text-lg tracking-tight font-extrabold text-maroon sm:mt-5 sm:text-2xl lg:mt-6 xl:text-4xl">
-                                <span className="block">G'day I'm Eric</span>
-                                <span className="block text-rich-black">& I'm a Test Engineer at Google</span>
+                                <span className="block">G&apos;day I&apos;m Eric</span>
+                                <span className="block text-rich-black">& I&apos;m a Test Engineer at Google</span>
                             </h1>
                             <p className="mt-3 text-base text-gray-400 sm:mt-5 sm:text-xl lg:text-lg xl:text-lg">
-                                Technology has been rapidly growing and is at the forefront of bringing change. I've always wanted to build
+                                Technology has been rapidly growing and is at the forefront of bringing change. I&apos;ve always wanted to build
                                 awesome & secure products which delight users and impact their lives. I currently build tools for our Pixel
                                 Security QA team at Google!
                             </p>
@@ -18,11 +18,9 @@ export default function Hero() {
                     </div>
                     <div className="mt-12 mb-0 sm:-mb-48 lg:m-0 lg:relative">
                         <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
-                            {/* Illustration taken from Lucid Illustrations: https://lucid.pixsellz.io/ */}
                             <img
                                 className="w-1/2 lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
                                 src="/images/transparent-eric-profile.png"
-
                                 alt=""
                             />
                         </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,27 +2,11 @@ import type { AppProps } from 'next/app'
 import React from 'react'
 
 import Link from 'next/link'
-import { PrismicProvider } from '@prismicio/react'
-import { PrismicPreview } from '@prismicio/next'
-import { linkResolver, repositoryName } from '../config/prismicio'
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <PrismicProvider
-    linkResolver={linkResolver}
-    internalLinkComponent={({ href, children, ...props }) => (
-      <Link href={href}>
-        <a {...props}>
-          {children}
-        </a>
-      </Link>
-    )}
-  >
-    <PrismicPreview repositoryName={repositoryName}>
-      <Component {...pageProps} />
-    </PrismicPreview>
-  </PrismicProvider>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
