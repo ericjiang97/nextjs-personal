bfd2ec16b01a9c63c3d6491a896c176d885db875
Eric Jiang <eric@ericjiang.dev>
2022-04-04 12:01:50 +0800

    fix more issues

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -64,20 +64,24 @@ export default function NavBar() {
 
                     <Disclosure.Panel className="sm:hidden">
                         <div className="px-2 pt-2 pb-3 space-y-1">
-                            {navigation.map((item) => (
-                                <Disclosure.Button
-                                    key={item.name}
-                                    as="a"
-                                    href={item.href}
-                                    className={classNames(
-                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-600 hover:bg-gray-700 hover:text-white',
-                                        'block px-3 py-2 rounded-md text-base font-medium'
-                                    )}
-                                    aria-current={item.current ? 'page' : undefined}
-                                >
-                                    {item.name}
-                                </Disclosure.Button>
-                            ))}
+                            {navigation.map((item) => {
+                                const isCurrent = currentPath === item.href
+
+                                return (
+                                    <Disclosure.Button
+                                        key={item.name}
+                                        as="a"
+                                        href={item.href}
+                                        className={classNames(
+                                            isCurrent ? 'bg-gray-900 text-white' : 'text-gray-600 hover:bg-gray-700 hover:text-white',
+                                            'block px-3 py-2 rounded-md text-base font-medium'
+                                        )}
+                                        aria-current={isCurrent ? 'page' : undefined}
+                                    >
+                                        {item.name}
+                                    </Disclosure.Button>
+                                )
+                            })}
                         </div>
                     </Disclosure.Panel>
                 </>
