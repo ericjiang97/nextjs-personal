0cd113500b9c851c19ea84453ed1df398d9b0dbe
Eric Jiang <eric@ericjiang.dev>
2022-09-09 12:06:29 +0800

    some small improvements to the slide control bar

--- components/PdfViewer/SlidesViewer.tsx
+++ components/PdfViewer/SlidesViewer.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 
-import { ChevronRightIcon, ChevronLeftIcon } from "@heroicons/react/solid";
+import {
+  ChevronRightIcon,
+  ChevronLeftIcon,
+  DownloadIcon,
+} from "@heroicons/react/solid";
 
 import {
-  Icon,
-  MinimalButton,
+  Button,
   Position,
   SpecialZoomLevel,
   Tooltip,
   Viewer,
 } from "@react-pdf-viewer/core";
 import {
+  NumberOfPagesProps,
   pageNavigationPlugin,
+  RenderCurrentPageLabelProps,
   RenderGoToPageProps,
+  RenderNumberOfPagesProps,
 } from "@react-pdf-viewer/page-navigation";
 
 import "@react-pdf-viewer/core/lib/styles/index.css";
@@ -27,7 +33,8 @@ const SlidesViewer: React.FC<SlidesViewerProps> = ({ fileUrl }) => {
   const pageNavigationPluginInstance = pageNavigationPlugin();
   const disableScrollPluginInstance = disableScrollPlugin();
 
-  const { GoToNextPage, GoToPreviousPage } = pageNavigationPluginInstance;
+  const { GoToNextPage, GoToPreviousPage, NumberOfPages, CurrentPageLabel } =
+    pageNavigationPluginInstance;
   return (
     <div className="rpv-core__viewer">
       <div
@@ -42,37 +49,56 @@ const SlidesViewer: React.FC<SlidesViewerProps> = ({ fileUrl }) => {
           defaultScale={SpecialZoomLevel.PageFit}
         />
       </div>
-      <div className="mt-2 flex w-full flex-row bg-gray-900">
-        <div>
-          <GoToPreviousPage>
-            {(props: RenderGoToPageProps) => (
-              <Tooltip
-                position={Position.BottomCenter}
-                target={
-                  <MinimalButton onClick={props.onClick}>
-                    <ChevronLeftIcon className="h-5 w-5 text-gray-400" />
-                  </MinimalButton>
-                }
-                content={() => "Previous page"}
-                offset={{ left: 0, top: 8 }}
-              />
-            )}
-          </GoToPreviousPage>
-        </div>
-        <GoToNextPage>
-          {(props: RenderGoToPageProps) => (
+      <div className="mt-2 flex w-full flex-row items-center bg-gray-900 px-4 py-2">
+        <GoToPreviousPage>
+          {({ onClick, isDisabled }: RenderGoToPageProps) => (
             <Tooltip
               position={Position.BottomCenter}
               target={
-                <MinimalButton onClick={props.onClick}>
+                <button
+                  onClick={onClick}
+                  disabled={isDisabled}
+                  className="mx-2 text-gray-100 disabled:text-gray-400"
+                >
+                  <ChevronLeftIcon className="h-5 w-5 text-gray-400" />
+                </button>
+              }
+              content={() => "Previous page"}
+              offset={{ left: 0, top: 8 }}
+            />
+          )}
+        </GoToPreviousPage>
+        <GoToNextPage>
+          {({ onClick, isDisabled }: RenderGoToPageProps) => (
+            <Tooltip
+              position={Position.BottomCenter}
+              target={
+                <button onClick={onClick} disabled={isDisabled}>
                   <ChevronRightIcon className="h-5 w-5 text-gray-400" />
-                </MinimalButton>
+                </button>
               }
               content={() => "Next page"}
               offset={{ left: 0, top: 8 }}
             />
           )}
         </GoToNextPage>
+        <CurrentPageLabel>
+          {(props: RenderCurrentPageLabelProps) => {
+            return (
+              <div className="flex flex-1 justify-center text-white">
+                <div>{`${props.currentPage} / ${props.numberOfPages}`}</div>
+              </div>
+            );
+          }}
+        </CurrentPageLabel>
+        <a
+          type="button"
+          href={fileUrl}
+          className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >
+          <DownloadIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+          Download
+        </a>
       </div>
     </div>
   );
