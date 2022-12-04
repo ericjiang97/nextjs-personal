d30ad5b6ea0becbe2f547b2ce325a525e08870b5
Eric Jiang <eric@ericjiang.dev>
2022-12-04 16:44:01 +0800

    fix flex breakpoint

--- slices/ImageWithTextonOneSide/index.tsx
+++ slices/ImageWithTextonOneSide/index.tsx
@@ -31,11 +31,11 @@ const ImageWithTextOnOneSide: React.FC<ImageWithTextOnOneSideProps> = ({
           <img src={image.url} alt={image.alt || ""} />
         </div>
       )}
-      <div className="flex-1 py-2 px-1">
+      <div className="min-w-[300px] flex-1 py-2 px-1">
         <PrismicRichTextWrapper data={text} />
       </div>
       {position === "right" && image.url && (
-        <div className="flex-1">
+        <div className="min-w-[300px] flex-1">
           <img src={image.url} alt={image.alt || ""} />
         </div>
       )}
