df9d9f17159fbe26ae29a22f90128b541df5c0a7
Eric <eric@ericjiang.dev>
2026-02-24 11:44:51 +1100

    test

--- app/reviews/[uid]/content.tsx
+++ app/reviews/[uid]/content.tsx
@@ -2,12 +2,9 @@
 
 import React, { Suspense, use } from 'react'
 import MainLayout from '../../../containers/MainLayout'
-import { FoodreviewsDocument } from '../../../prismicio-types'
 import moment from 'moment'
 import Rating from '../../../components/Rating/rating'
 import PrismicRichTextWrapper from '../../../components/PrismicRichTextWrapper'
-import MapsDisplay from '../../../components/MapsDisplay'
-import GmapsApiProvider from '../../../providers/GMapsProvider';
 
 declare interface ReviewContentProps {
     review: Promise<FoodreviewsDocument>
@@ -24,34 +21,31 @@ export default function ReviewContent({
     const formattedDate = moment(review_date).fromNow();
 
     return (
-        <GmapsApiProvider>
-            <MainLayout
-                pageTitle={title}
-                customHero={
-                    <div className="flex flex-col gap-2">
-                        <span className="text-start block max-w-md text-base font-semibold uppercase tracking-wide text-gray-500 dark:text-gray-400">
-                            {`Last Visited: ${formattedDate}`}
+        <MainLayout
+            pageTitle={title}
+            customHero={
+                <div className="flex flex-col gap-2">
+                    <span className="text-start block max-w-md text-base font-semibold uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                        {`Last Visited: ${formattedDate}`}
+                    </span>
+                    <h1>
+                        <span className="text-start mt-2 block text-3xl font-extrabold leading-8 tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+                            {name}
                         </span>
-                        <h1>
-                            <span className="text-start mt-2 block text-3xl font-extrabold leading-8 tracking-tight text-gray-900 dark:text-white sm:text-4xl">
-                                {name}
-                            </span>
-                        </h1>
-                        <h2>
-                            <Rating rating={Number(rating)} />
-                        </h2>
-                    </div>
-                }
-                pageMeta={{
-                    description: title,
-                    endpoint: `/reviews/${reviewData.uid}`,
-                }}
-            >
-                <div className="px-6">
-                    {maps_url && <MapsDisplay mapsUrl={maps_url.text} />}
-                    <PrismicRichTextWrapper data={reviewData.data.review} />
+                    </h1>
+                    <h2>
+                        <Rating rating={Number(rating)} />
+                    </h2>
                 </div>
-            </MainLayout>
-        </GmapsApiProvider>
+            }
+            pageMeta={{
+                description: title,
+                endpoint: `/reviews/${reviewData.uid}`,
+            }}
+        >
+            <div className="px-6">
+                <PrismicRichTextWrapper data={reviewData.data.review} />
+            </div>
+        </MainLayout>
     )
 }
